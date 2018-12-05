3d603db7a2af2264e577018ea86640834105a293
Tom Edds <tom.edds@gmail.com>
2018-12-05 06:53:58 -0500

    add Swagger to configuration

--- src/main/groovy/name/edds/mileageservice/MileageConfiguration.groovy
+++ src/main/groovy/name/edds/mileageservice/MileageConfiguration.groovy
@@ -1,11 +1,18 @@
 package name.edds.mileageservice
 
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.ComponentScan
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.servlet.config.annotation.CorsRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+import springfox.documentation.builders.PathSelectors
+import springfox.documentation.builders.RequestHandlerSelectors
+import springfox.documentation.spi.DocumentationType
+import springfox.documentation.spring.web.plugins.Docket
+import springfox.documentation.swagger2.annotations.EnableSwagger2
 
 @Configuration
+@EnableSwagger2
 @ComponentScan(basePackages = "name.edds.mileageservice")
 class MileageConfiguration implements WebMvcConfigurer{
 
@@ -14,4 +21,14 @@ class MileageConfiguration implements WebMvcConfigurer{
         registry.addMapping("/api/**").allowedOrigins("http://localhost:8080");
     }
 
+    @Bean
+    Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
+    }
+
 }
+
